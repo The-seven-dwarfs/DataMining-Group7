f7d920b08d2b53b5170dbffcda41d288e65a02e9
bendico765 <bendico765@gmail.com>
2022-10-13 19:17:29 +0200

    Finished (?) cleaning code for users dataset

--- utils.py
+++ utils.py
@@ -1,19 +1,6 @@
 from math import log, ceil
-from datetime import datetime
 import shutil as shl
-
-import pandas
-
-def is_datetime_format_correct(date_string: str, expected_format: str)->bool:
-	"""
-	Checks if the string date_string is representing a datetime in the format
-	expressed by expected_format
-	"""
-	try:
-		datetime.strptime(date_string, expected_format)
-		return True
-	except ValueError:
-		return False
+import pandas as pd
 
 def get_sturges_bins(sample_size: int)->float:
 	"""
@@ -26,8 +13,8 @@ def unpack():
 	shl.unpack_archive("dataset/users.zip", "dataset") # unpacks the users.zip into the datasets folder (The users zip is small enough to be commited to github if we would like)
 	shl.unpack_archive("dataset/tweets.zip", "dataset") # unpacks the tweets.zip into the datasets folder
 
-def repair_lang_attribute(users_df: pandas.DataFrame):
-	wrong_fields = ["Select Language...", "xx-lc"] # only 3 elements
+def repair_lang_attribute(users_df: pd.DataFrame):
+	# wrong_fields = ["Select Language...", "xx-lc"] # only 3 elements
 	to_map_fields = {
 		"en-gb": "en-GB",
 		"zh-tw": "zh-TW",
@@ -36,9 +23,11 @@ def repair_lang_attribute(users_df: pandas.DataFrame):
 	}
 
 	# dropping wrong fields
+	"""
 	wrong_index = lambda x: True if x[1] in wrong_fields else False
 	wrong_indexes = [index for (index, _) in filter(wrong_index, enumerate(users_df["lang"]))]
-	#users_df.drop(index=wrong_indexes, inplace=True)
+	users_df.drop(index=wrong_indexes, inplace=True)
+	"""
 
 	# mapping incorrect values to fixed ones
 	for language in to_map_fields:
