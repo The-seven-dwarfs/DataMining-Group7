f7264c640b4bd9cf30143b2306308813b206e7a3
matteotolloso <matteotolloso@gmail.com>
2022-11-05 14:22:19 +0100

    progress in language model

--- utils.py
+++ utils.py
@@ -62,35 +62,41 @@ def repair_lang_attribute(users_df: pd.DataFrame):
 			users_df.loc[index,"lang"] = to_map_fields[old_language]
 
 
-def concurrent_language_model(dataset : pd.DataFrame) -> pd.DataFrame:
+def concurrent_language_model(dataset : pd.DataFrame, parallelism_level : int) -> pd.DataFrame:
 
 	def process_function(datset_part_number : int, dataset_part : pd.DataFrame, return_queue : mp.Queue) -> pd.DataFrame:
+		print("Process number ", datset_part_number, " started")
 		# from all language: all avaiable languages
 		# with low accuracy: faster, without this it takes at least 1 second for every tweet (but even more if the text is long)
 		# with preload language model: slower in building the model but faster in the prediction
 		detector = LanguageDetectorBuilder.from_all_languages().with_low_accuracy_mode().with_preloaded_language_models().build()
-		dataset_part["tweet_lang"] = [detector.detect_language_of(text) for text in dataset_part["text"]]
+		dataset_part["tweet_lang"] = [(detector.detect_language_of(text)) for text in dataset_part["text"]]
 		return_queue.put( (dataset_part, datset_part_number) )
+		print("Process number", datset_part_number, "finished")
 
-	num_process = 12
+
+	num_process = parallelism_level
 	process_list = []
 	splitted_dataset = np.array_split(dataset, num_process)
 	concurrent_queue = mp.Queue()
 	
 	
-	for i in range(num_process):
+	for i in range(len(splitted_dataset)):
 		process_list.append(mp.Process(target=process_function, args=( i, splitted_dataset[i] , concurrent_queue,  )))
-	
+
 	for i in process_list:
 		i.start()
 	
 	for i in process_list:
 		i.join()
 
-	
+	print("All processes joined")
+
 	splitted_dataset = []
-	for i in range(num_process):
+	for i in range(len(process_list)):
 		splitted_dataset.append(concurrent_queue.get(block=False))
+
+	print("All computation retrived")
 
 	splitted_dataset.sort(key = lambda x : x[1])
 	splitted_dataset = ( i for (i, _) in splitted_dataset)
