df3e32eb1512679026cdd242cae764924137af11
matteotolloso <matteotolloso@gmail.com>
2022-11-05 16:02:07 +0100

    language model with high concurrency finally working

--- utils.py
+++ utils.py
@@ -65,7 +65,6 @@ def repair_lang_attribute(users_df: pd.DataFrame):
 def concurrent_language_model(dataset : pd.DataFrame, parallelism_level : int) -> pd.DataFrame:
 
 	def process_function(datset_part_number : int, dataset_part : pd.DataFrame, return_queue : mp.Queue) -> pd.DataFrame:
-		print("Process number ", datset_part_number, " started")
 		# from all language: all avaiable languages
 		# with low accuracy: faster, without this it takes at least 1 second for every tweet (but even more if the text is long)
 		# with preload language model: slower in building the model but faster in the prediction
@@ -80,27 +79,27 @@ def concurrent_language_model(dataset : pd.DataFrame, parallelism_level : int) -
 	splitted_dataset = np.array_split(dataset, num_process)
 	concurrent_queue = mp.Queue()
 	
-	
 	for i in range(len(splitted_dataset)):
 		process_list.append(mp.Process(target=process_function, args=( i, splitted_dataset[i] , concurrent_queue,  )))
 
 	for i in process_list:
 		i.start()
-	
-	for i in process_list:
-		i.join()
-
-	print("All processes joined")
 
 	splitted_dataset = []
-	for i in range(len(process_list)):
-		splitted_dataset.append(concurrent_queue.get(block=False))
 
-	print("All computation retrived")
+	print("All", num_process, "processes runnig ...")
+	
+	for i in range(len(process_list)):
+		print("Trying to collect", i + 1, "/", len(process_list), "of information")
+		splitted_dataset.append(concurrent_queue.get(block=True))
+
+	print("All computation retreived, merging spitted datasets")
 
 	splitted_dataset.sort(key = lambda x : x[1])
 	splitted_dataset = ( i for (i, _) in splitted_dataset)
 	dataset = pd.concat(splitted_dataset)
 	dataset.reset_index(drop=True, inplace=True)
+
+	print("Merging completed")
 	return dataset
 
